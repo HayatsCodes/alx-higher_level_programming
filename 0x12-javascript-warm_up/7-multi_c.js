fe5c4cb68ae39b263123d3f149aec05fbde508d4
Hayatudeen Abdulrahman <hayatudeenabdulrahman@gmail.com>
2022-11-29 02:45:34 +0000

    style fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -6,11 +6,10 @@ const args = process.argv.slice(2, 3);
 if (!(args[0]) || isNaN(args[0])) {
   console.log('Missing number of occurrences');
 } else if (!(isNaN(args[0]))) {
-    let toNum = +(args[0]);
-    toNum = Math.trunc(toNum);
-    let i = 0;
-    for (; i < toNum; i++) {
-      console.log('C is fun');
-    }
+  let toNum = +(args[0]);
+  toNum = Math.trunc(toNum);
+  let i = 0;
+  for (; i < toNum; i++) {
+    console.log('C is fun');
+  }
 }
-
