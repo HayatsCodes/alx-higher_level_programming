4dcdc3adc3e6872b4690f9da6e49be47c0902aad
Hayatudeen Abdulrahman <hayatudeenabdulrahman@gmail.com>
2022-11-29 01:55:30 +0000

    Trying again

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -7,5 +7,5 @@ if (!(args[0]) || isNaN(args[0])) {
   console.log('Not a number');
 } else if (!(isNaN(args[0]))) {
   const toNum = +(args[0]);
-  console.log('My number: ', Math.trunc(toNum));
+  console.log('My number:', Math.trunc(toNum));
 }
