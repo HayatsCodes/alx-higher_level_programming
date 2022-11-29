6352e2d507a66a15c9d1fc07aef324b636bfb497
Hayatudeen Abdulrahman <hayatudeenabdulrahman@gmail.com>
2022-11-29 02:16:24 +0000

    semistandard passed

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 
 const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (i = 0; i < 3; i++) {
+let i = 0;
+
+while (i < 3) {
   console.log(myArray[i]);
+  i++;
 }
