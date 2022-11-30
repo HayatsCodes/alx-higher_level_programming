d2bd239a3ad6fa84fd46f304854d0799517b86e5
Hayatudeen Abdulrahman <hayatudeenabdulrahman@gmail.com>
2022-11-30 09:21:54 +0000

    Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const process = require('process');
+let args = process.argv.slice(2, 3);
+
+function fact (a) {
+  if (a === 1) {
+    return (1);
+  }
+  return (a * fact(a - 1));
+}
+
+if (isNaN(args[0])) {
+  args[0] = 1;
+} else if (!(isNaN(args[0]))) {
+  args[0] = +(args[0]);
+}
+
+console.log(fact(args[0]));
